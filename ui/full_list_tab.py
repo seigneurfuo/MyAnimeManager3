cf48138160f19708fc280e265b273e7db5f41792
seigneurfuo <seigneurfuo@protonmail.com>
2020-04-05 17:15:38 +0200

    Update full_list_tab.py and full_list_tab.ui

--- ui/full_list_tab.py
+++ ui/full_list_tab.py
@@ -15,6 +15,9 @@ class FullListTab(QWidget):
         self.init_ui()
         self.init_events()
 
+        self.current_serie_id = None
+        self.current_season_id = None
+
         self.fill_series_combobox()
 
     def init_ui(self):
@@ -24,7 +27,52 @@ class FullListTab(QWidget):
     def init_events(self):
         self.comboBox.currentIndexChanged.connect(self.on_series_list_current_index_changed)
         # self.pushButton.clicked.connect(self._on_serie_edit)
+        self.tableWidget.currentItemChanged.connect(self.on_seasons_list_current_index_changed)
+
+        # region ----- Boutons -----
+        self.delete_serie_button.clicked.connect(self.on_delete_serie_button_clicked)
+        # endregion
+
         pass
+
+    # region ----- Serie combobox -----
+    def fill_series_combobox(self):
+        self.comboBox.clear()
+        for serie in Series.select().order_by(Series.sort_id):
+            self.comboBox.addItem(serie.name)
+    # endregion
+
+    def on_series_list_current_index_changed(self, index):
+        # ----- -----
+        # FIXME: Changer de le mode détection de la série
+        serie = Series.select().where(Series.name == self.comboBox.currentText()).get()
+        self.current_serie_id = serie.id_
+        self.fill_serie_data(serie)
+        self.fill_season_list(serie)
+
+    # region ----- Remplissage de la liste des informations sur la série -----
+    def fill_serie_data(self, serie):
+        fields = [(self.label_3, serie.name)]
+        for field, value in fields:
+            field.setText(value)
+
+        self.plainTextEdit.setPlainText(serie.description)
+    # endregion
+
+    def on_delete_serie_button_clicked(self):
+        # FIXME: Cascade SQL Delete
+
+        # ----- Supression des saisons -----
+        seasons = Seasons.select().where(Seasons.serie == self.current_serie_id).order_by(Seasons.sort_id)
+        print([season.name for season in seasons])
+        for season in seasons:
+            Seasons.delete_by_id(season.id_)
+
+        # ----- Supression de la série -----
+        Series.delete_by_id(self.current_serie_id)
+
+        self.fill_series_combobox()
+        self.fill_serie_data()
 
     # region ----- Remplissage de la liste des saisons -----
     def fill_season_list(self, serie):
@@ -36,30 +84,18 @@ class FullListTab(QWidget):
         self.tableWidget.setRowCount(len(seasons))
 
         for row_id, season in enumerate(seasons):
-
-
-
-            self.tableWidget.setItem(row_id, 0, QTableWidgetItem(season.name))
+            self.tableWidget.setItem(row_id, 0, QTableWidgetItem(season.seasons_type.name))
             self.tableWidget.setItem(row_id, 1, QTableWidgetItem(season.name))
     # endregion
 
-    # region ----- Remplissage de la liste des informations sur la série -----
-    def fill_serie_data(self, serie):
-        fields = [(self.label_3, serie.name)]
-        for field, value in fields:
-            field.setText(value)
+    def fill_season_data(self, season):
+        print(season.name)
 
-        self.plainTextEdit.setPlainText(serie.description)
-    # endregion
+    def on_seasons_list_current_index_changed(self):
+        # TODO:
+        #season = Series.select().where(Series.name == self.comboBox.currentText()).get()
+        # self.current_season_id = season.id_
 
-    # region ----- Serie combobox -----
-    def fill_series_combobox(self):
-        self.comboBox.clear()
-        for serie in Series.select().order_by(Series.sort_id):
-            self.comboBox.addItem(serie.name)
+        pass
 
-    def on_series_list_current_index_changed(self, index):
-        serie = Series.select().where(Series.name == self.comboBox.currentText()).get()
-        self.fill_serie_data(serie)
-        self.fill_season_list(serie)
     # endregion

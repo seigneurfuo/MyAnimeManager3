157bd06f69d5081980c014f45ef61d15a842b4d8
seigneurfuo <seigneurfuo@protonmail.com>
2022-01-21 17:00:29 +0100

    Ajout d'une fenetre de dialogue pour avoir des infos sur la version de Python et autres composants.

---
+++ src/ui/dialogs/about.py
@@ -0,0 +1,36 @@
+import os
+from platform import python_version
+
+from PyQt5.QtCore import QT_VERSION_STR, PYQT_VERSION_STR
+from PyQt5.QtWidgets import QDialog
+from PyQt5.uic import loadUi
+
+from peewee import __version__ as peewee_version
+
+
+class About(QDialog):
+    def __init__(self):
+        super(About, self).__init__()
+
+        self.init_ui()
+        self.init_events()
+
+
+    def init_ui(self):
+        loadUi(os.path.join(os.path.dirname(__file__), 'about.ui'), self)
+
+        self.fill_data()
+
+    def init_events(self):
+        pass
+
+
+    def fill_data(self):
+        fields = [(self.application_version, ""),
+                  (self.python_version, python_version()),
+                  (self.qt_version, QT_VERSION_STR),
+                  (self.pyqt_version, PYQT_VERSION_STR),
+                  (self.peewee_version, peewee_version)]
+
+        for field, value in fields:
+            field.setText(value)

2bf146dd04b9619eaff82f6ac8866b54fb492976
seigneurfuo <seigneurfuo@protonmail.com>
2023-04-14 00:08:10 +0200

    Modification de noms dans l'écran des statistiques.

--- src/ui/tabs/stats_tab.py
+++ src/ui/tabs/stats_tab.py
@@ -1,9 +1,8 @@
 #!/bin/env python3
 import random
 
-from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QHeaderView, QMessageBox
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QMessageBox
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import QTime
 
 import os
 
@@ -19,34 +18,36 @@ class StatsTab(QWidget):
 
         self.parent = parent
 
-        self.request = []
-
         self.init_ui()
         self.init_events()
 
     def init_ui(self):
         loadUi(os.path.join(os.path.dirname(__file__), "stats.ui"), self)
 
-        requests_list = [self.tr("Nombre de saisons par année de sortie"), self.tr("Saisons les plus revisionnées"),
+        # Remplissage la liste des extractions
+        queries_list = [self.tr("Nombre de saisons par année de sortie"), self.tr("Saisons les plus revisionnées"),
                          self.tr("Saison avec le plus d'épisodes"), self.tr("Nombre d'épisodes vus par année"),
                          self.tr("Séries avec le plus d'épisodes")]
-        for index, name in enumerate(requests_list):
+        for index, name in enumerate(queries_list):
             self.comboBox.addItem(name, userData=index)
+
 
     def init_events(self):
         self.comboBox.currentIndexChanged.connect(self.when_stats_list_current_index_changed)
         self.export_button.clicked.connect(self.when_export_button_clicked)
 
     def when_visible(self):
-        self.fill_stats_table_data()
+        self.fill_data()
 
     def when_stats_list_current_index_changed(self):
-        self.fill_stats_table_data()
+        self.fill_data()
 
-    def fill_stats_table_data(self):
-        headers, request = self.execute_request()
+    def fill_data(self):
+        headers, data = self.execute_query()
+        self.fill_stats_table(headers, data)
 
-        row_count = len(request)
+    def fill_stats_table(self, headers, data):
+        row_count = len(data)
         self.label.setText(self.tr("Nombre d'éléments: ") + str(row_count))
         self.stats_table.setRowCount(row_count)
         self.stats_table.setColumnCount(len(headers))
@@ -54,7 +55,7 @@ class StatsTab(QWidget):
        # Headers
         self.stats_table.setHorizontalHeaderLabels(headers)
 
-        for row_index, row in enumerate(request):
+        for row_index, row in enumerate(data):
             for col_index, value in enumerate(row.values()):
                 item = QTableWidgetItem(str(value))
                 item.setToolTip(item.text())
@@ -64,48 +65,49 @@ class StatsTab(QWidget):
         # self.stats_table.horizontalHeader().setSectionResizeMode(self.stats_table.columnCount() - 1,
         #                                                          QHeaderView.ResizeToContents)
 
-    def when_export_button_clicked(self):
-        request_name = self.comboBox.currentText()
 
-        filepath = utils.export_qtablewidget(self.stats_table, self.parent.parent.profile.path, request_name)
+    def when_export_button_clicked(self):
+        query_name = self.comboBox.currentText()
+
+        filepath = utils.export_qtablewidget(self.stats_table, self.parent.parent.profile.path, query_name)
         # TODO: Bouton pour ouvrir le dossier ?
         QMessageBox.information(self, self.tr("Export terminé"),
                                 self.tr("Le fichier a été généré ici:") + "\n    " + filepath,
                                 QMessageBox.Ok)
 
-    def execute_request(self):
-        request_index = self.comboBox.currentData()
+    def execute_query(self):
+        query_index = self.comboBox.currentData()
 
         # Nombre de saisons par année
-        if request_index == 0:
+        if query_index == 0:
             headers = ["Année", "Nombre"]
-            request = Seasons.select(Seasons.year, peewee.fn.COUNT(Seasons.id)).where(Seasons.is_deleted == 0)\
+            query = Seasons.select(Seasons.year, peewee.fn.COUNT(Seasons.id)).where(Seasons.is_deleted == 0)\
                 .group_by(Seasons.year).order_by(Seasons.year).dicts()
 
         # Saisons les plus revisionnées
-        elif request_index == 1:
+        elif query_index == 1:
             headers = ["Série", "Saison", "Nombre"]
-            request = Seasons.select(Series.name.alias("serie_name"), Seasons.name, Seasons.view_count).join(Series)\
+            query = Seasons.select(Series.name.alias("serie_name"), Seasons.name, Seasons.view_count).join(Series)\
                 .where(Seasons.is_deleted == 0).order_by(Seasons.view_count.desc()).dicts()
 
         # Saison avec le plus d'épisodes
-        elif request_index == 2:
+        elif query_index == 2:
             headers = ["Série", "Saison", "Nombre"]
-            request = Seasons.select(Series.name.alias("serie_name"), Seasons.name, Seasons.episodes).join(Series)\
+            query = Seasons.select(Series.name.alias("serie_name"), Seasons.name, Seasons.episodes).join(Series)\
                 .where(Seasons.is_deleted == 0).order_by(Seasons.episodes.desc()).dicts()
 
         #
-        elif request_index == 3:
+        elif query_index == 3:
             headers = ["Année", "Nombre"]
-            request = Planning.select(Planning.date.year, peewee.fn.COUNT(Planning.id))\
+            query = Planning.select(Planning.date.year, peewee.fn.COUNT(Planning.id))\
                 .group_by(Planning.date.year).order_by(Planning.date.year).dicts()
 
         # Séries avec le plus d'épisodes
-        elif request_index == 4:
+        elif query_index == 4:
             headers = ["Série", "Nombre saisons", "Nombre épisodes"]
-            request = Seasons.select(Series.name.alias("serie_name"), peewee.fn.COUNT(Seasons.id),
+            query = Seasons.select(Series.name.alias("serie_name"), peewee.fn.COUNT(Seasons.id),
                                      peewee.fn.SUM(Seasons.episodes)).join(Series)\
                 .where(Seasons.is_deleted == 0).group_by(Series.id)\
                 .order_by(peewee.fn.SUM(Seasons.episodes).desc()).dicts()
 
-        return headers, request
+        return headers, query
